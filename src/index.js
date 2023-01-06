0e1861a1f0c1b97b8a2d79508b05d524d1e34862
Hiranmoy Mandal <94227277+Hiranmoy45@users.noreply.github.com>
2023-01-07 00:33:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App';
 import { ModalProvider } from './context/modal-context';
 import { ThemeProvider } from './context/theme-context';
+import { HashRouter } from "react-router-dom";
+
 import './index.css'
 
 
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
     <ThemeProvider>
     <ModalProvider>
-        <App/>
+         <HashRouter>
+       <App />
+    </HashRouter>
     </ModalProvider>
     </ThemeProvider>
 );
